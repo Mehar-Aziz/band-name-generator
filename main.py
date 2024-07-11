bf5a81fb8ac4c0a41cba25c3562743afc95e912d
Mehar-Aziz <meharaziz.1614>
2024-07-11 18:34:56 +0500

    swapping

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 #this code will take input first and then will display the length of the name
-print(len(input("What is your name? ")))
+#print(len(input("What is your name? ")))
+
+#swapping
+a = input("a = ")
+b = input ("b = ")
+
+c = a
+a = b
+b = c
+
+print ("a = "+ a)
+print ("b = "+ b)
